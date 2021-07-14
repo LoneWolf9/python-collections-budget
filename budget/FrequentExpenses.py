6a5df452331db4cf1fef80855ab7f8df5641b471
LoneWolf9 <lstdon9@yahoo.com>
2021-07-14 14:55:40 -0600

    finishing up steps in module 2

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,6 +1,7 @@
 from . import Expense
 
 import collections
+importmatplotlib.pyplot as plt
 
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
@@ -14,3 +15,8 @@ print(spending_counter)
 
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
+
+fig, ax = plt.subplots()
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
