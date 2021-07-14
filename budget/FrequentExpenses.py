de7cae748bafa0b356425302750e3fa1830f07d0
LoneWolf9 <lstdon9@yahoo.com>
2021-07-14 14:50:26 -0600

    finishing step 6 in module 1

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,8 +1,16 @@
 from . import Expense
 
+import collections
+
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
+
+spending_counter = collections.Counter(spending_categories)
+print(spending_counter)
+
+top5 = spending_counter.most_common(5)
+categories, count = zip(*top5)
