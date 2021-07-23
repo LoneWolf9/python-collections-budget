9edcfc2a9d2376c1573f2a13a8092e2abc754128
LoneWolf9 <lstdon9@yahoo.com>
2021-07-23 12:30:19 -0600

    checking work after adding apend method

---
+++ budget/BudgetList.py
@@ -0,0 +1,13 @@
+class BudgetList:
+
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def __append__(self, item):
+        if(self.sum_expenses & & item) > self.budget:
+            append(self.expenses[item])
+            self.sum_expenses += item
