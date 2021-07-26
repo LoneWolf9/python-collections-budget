449676ece6e21d379fc94345741fbca69848a370
LoneWolf9 <lstdon9@yahoo.com>
2021-07-26 10:16:36 -0600

    rolling back some code

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -29,9 +29,6 @@ class BudgetList:
     def __next__(self):
         try:
             return __next__(self.iter_e)
-        except:
-            StopIteration as stop
-            return __next__(self.iter_o)
 
 
 def main():
