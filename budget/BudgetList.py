0551c25c93fd23926ff24032dc5414467503bbb5
LoneWolf9 <lstdon9@yahoo.com>
2021-07-26 10:06:23 -0600

    completing 1, module 4

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -21,6 +21,9 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __iter__(self):
+        pass
+
 
 def main():
     myBudgetList = BudgetList(1200)
