2996510ac8d2fa8b4cf12a475debaa2f995626a5
LoneWolf9 <lstdon9@yahoo.com>
2021-07-29 09:12:23 -0600

    created food_expenses

--- budget/Expense.py
+++ budget/Expense.py
@@ -46,4 +46,9 @@ class Expenses():
         necessary_expenses = {
             x for x in self.list
             if x.category == 'Phone' or x.category == 'Auto and Gas' or x.category == 'Classes' or x.category == 'Utilities' or x.category == 'Mortgage'
+
+        }
+        food_expenses = {
+            x for x in self.list
+            if x.category == 'Groceries' or x.category == 'Eating Out'
         }
